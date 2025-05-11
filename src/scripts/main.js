f3910feb1fe4a192c3b6a677844376cdbb39254a
Csillag61 <csillagok@gmail.com>
2025-05-11 13:39:20 -0400

    sorted table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', function () {
+  const table = document.querySelector('table');
+  const tableBody = table.querySelector('tbody');
+  const headers = table.querySelectorAll('thead th');
+
+  headers.forEach((header, index) => {
+    header.addEventListener('click', () => {
+      const rows = Array.from(tableBody.querySelectorAll('tr'));
+      const isNumeric = !isNaN(
+        rows[0].cells[index].textContent.replace(/\$|,/g, ''),
+      );
+
+      rows.sort((rowA, rowB) => {
+        const cellA = rowA.cells[index].textContent.trim();
+        const cellB = rowB.cells[index].textContent.trim();
+
+        return isNumeric
+          ? parseFloat(cellA.replace(/\$|,/g, '')) -
+              parseFloat(cellB.replace(/\$|,/g, ''))
+          : cellA.localeCompare(cellB);
+      });
+      tableBody.innerHTML = ''; // Clear the table body
+      rows.forEach((row) => tableBody.appendChild(row));
+    });
+  });
+});
